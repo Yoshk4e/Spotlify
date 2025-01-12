7da1954c752798cc424e5ead4dbc53514e4cc2bd
Ahmed Osama <doliest4455@gmail.com>
2025-01-12 22:32:24 +0200

    oraganized

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include "SpotifyAPI.h"
+
+int main() {
+	
+	std::string cID, cSECRET;
+	std::cout << "Enter CLIENT_ID: ";
+	std::getline(std::cin >> std::ws, cID);
+	std::cout << "Enter CLIENT_SECRET: ";
+	std::getline(std::cin >> std::ws, cSECRET);
+
+	std::string postData = "grant_type=client_credentials&client_id=" + cID + "&client_secret=" + cSECRET;
+	getAccessToken(postData);
+
+
+}
